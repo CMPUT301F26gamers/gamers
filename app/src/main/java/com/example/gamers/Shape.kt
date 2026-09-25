17d8930a47b899bbf99d1f3caa05035db9ed6fc8
Melody Morris <156554171+AmitMorris@users.noreply.github.com>
2026-09-25 21:54:53 +0000

    Update Shape.kt

--- app/src/main/java/com/example/gamers/Shape.kt
+++ app/src/main/java/com/example/gamers/Shape.kt
@@ -1,3 +1,3 @@
 package com.example.gamers
 
-abstract class Shape(val x: Int, val y: Int, val color: String = "blue")
+abstract class Shape(val x: Int, val y: Int, val color: String = "PUT-COLOUR-HERE")
